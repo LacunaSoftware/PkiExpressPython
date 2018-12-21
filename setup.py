80e56007780f29e56596619d114e9b77dc34600a
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2018-12-21 18:20:04 -0200

    Incremented version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme.close()
 
 setup(
     name='pkiexpress',
-    version='1.3.1',
+    version='1.3.2',
     author='Ismael Medeiros',
     author_email="IsmaelM@lacunasoftware.com",
     description='Classes to consume PKI Express',
